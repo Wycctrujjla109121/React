d49715ceeb6966ef283925325e6fba0d97a39a2d
Wycctrujjla109121 <56939296+Wycctrujjla109121@users.noreply.github.com>
2022-03-14 01:36:46 +0300

    react route

--- src/components/slidebar/slidebar.jsx
+++ src/components/slidebar/slidebar.jsx
@@ -1,25 +1,26 @@
+import { NavLink, Link } from 'react-router-dom'
 import '../slidebar/slidebar.css'
 
-const Slidebar = () =>{
-    return(
-        <nav className='slidebar'>
-        <div className='slidebar__item'>
-          <a href='#' className='slidebar__link'>Profile</a>
-        </div>
-        <div className='slidebar__item'>
-          <a href='#' className='slidebar__link'>Message</a>
-        </div>
-        <div className='slidebar__item'>
-          <a href='#' className='slidebar__link'>News</a>
-        </div>
-        <div className='slidebar__item'>
-          <a href='#' className='slidebar__link'>Music</a>
-        </div>
-        <div className='slidebar__item'>
-          <a href='#' className='slidebar__link'>Settings</a>
-        </div>
-      </nav>
-    );
+const Slidebar = () => {
+  return (
+    <nav className='slidebar'>
+      <div className='slidebar__item'>
+        <NavLink to='/profile' className='slidebar__link'>Profile</NavLink>
+      </div>
+      <div className='slidebar__item'>
+        <NavLink to='/dialogs' className='slidebar__link'>Message</NavLink>
+      </div>
+      <div className='slidebar__item'>
+        <NavLink to='/news' className='slidebar__link'>News</NavLink>
+      </div>
+      <div className='slidebar__item'>
+        <NavLink to='/music' className='slidebar__link'>Music</NavLink>
+      </div>
+      <div className='slidebar__item'>
+        <NavLink to='/settings' className='slidebar__link'>Settings</NavLink>
+      </div>
+    </nav>
+  );
 }
 
 export default Slidebar;
